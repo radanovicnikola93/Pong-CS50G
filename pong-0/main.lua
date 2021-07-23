0ce4e67053cf9bb3517dd5315f197d86c56059a4
Nikola <radanovicnikola93@gmail.com>
2021-07-23 19:14:41 +0200

    Added comment

--- pong-0/main.lua
+++ pong-0/main.lua
@@ -56,7 +56,7 @@ function love.draw()
     love.graphics.printf(
         'Hello Pong!',                          -- text to render
         0,                                            -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,   --starting Y (halfway down the screen)
+        WINDOW_HEIGHT / 2 - 6,   --starting Y (halfway down the screen), the default font size in LOVE2D is 12px
         WINDOW_WIDTH,               -- number of pixels to center within (the entire screen here)
         'center')                                   -- alignment mode, can be 'center', 'left', or 'right'
 end
