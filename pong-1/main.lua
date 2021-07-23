0a156e68306d65aa5517cc8b7824f0d4d611e27b
Nikola <radanovicnikola93@gmail.com>
2021-07-23 19:56:34 +0200

    Added functions

--- pong-1/main.lua
+++ pong-1/main.lua
@@ -31,3 +31,48 @@ WINDOW_WIDTH = 1280
 VIRTUAL_HEIGHT = 243
 VIRTUAL_WIDTH = 432
 
+--[[
+    Runs when the game first starts up, only once; used to initialize the game.
+]]
+function love.load()
+    -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text 
+    -- and graphics; try removing this function to see the difference!
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- initialize our virtual resolution, which will be rendered within our
+    -- actual window no matter its dimensions; replaces our love.window.setMode call
+    -- from the last example
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+--[[
+    Keyboard handling, called by LÖVE2D each frame; 
+    passes in the key we pressed so we can access.
+]]
+function love.keypressed(key)
+    -- keys can be accessed by string name
+    if key == 'escape' then
+        -- function LÖVE gives us to terminate application
+        love.event.quit()
+    end
+end
+
+--[[
+    Called after update by LÖVE2D, used to draw anything to the screen, 
+    updated or otherwise.
+]]
+function love.draw()
+    -- begin rendering at virtual resolution
+    push:apply('start')
+
+    -- condensed onto one line from last example
+    -- note we are now using virtual width and height now for text placement
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end rendering at virtual resolution
+    push:apply('end')
+end
